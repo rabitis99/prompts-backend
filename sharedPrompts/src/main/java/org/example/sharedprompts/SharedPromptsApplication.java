c39bb345a3e5d94c353981f88bb8da257a2e0bda
rabitis99 <kyungsu9914@gmail.com>
2025-11-22 17:37:44 +0900

    chore: Spring Boot 프로젝트 초기 설정(Java 17, Gradle, Redis, MySQL)

---
+++ sharedPrompts/src/main/java/org/example/sharedprompts/SharedPromptsApplication.java
@@ -0,0 +1,13 @@
+package org.example.sharedprompts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedPromptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedPromptsApplication.class, args);
+	}
+
+}
---
+++ sharedPrompts/src/test/java/org/example/sharedprompts/SharedPromptsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sharedprompts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedPromptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
