25ee3384cea4cafafef8394b9f978279475327c6
rabitis99 <kyungsu9914@gmail.com>
2026-01-27 17:06:33 +0900

    패키지명 오타 수정 - ShedLock LockProvider 설정에서 @Primary 중복 선언 제거 - 조건부(@ConditionalOnProperty) 구조에서 항상 하나의 LockProvider만 등록되도록 개선 - 설정 의도를 명확히 하고 향후 Bean 충돌 가능성 제거

--- sharedPrompts/src/main/java/org/example/sharedprompts/global/config/scheduler/ShedLockTableInitializer.java
+++ sharedPrompts/src/main/java/org/example/sharedprompts/global/config/scheduler/ShedLockTableInitializer.java
@@ -1,4 +1,4 @@
-package org.example.sharedprompts.global.config.shcduler;
+package org.example.sharedprompts.global.config.scheduler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
