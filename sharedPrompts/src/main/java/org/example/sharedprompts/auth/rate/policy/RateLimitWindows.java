5fb84069bc77deea37d69687344d9ae4c63f20f9
rabitis99 <kyungsu9914@gmail.com>
2026-01-30 16:47:13 +0900

    insert 차단

--- sharedPrompts/src/main/java/org/example/sharedprompts/auth/rate/policy/RateLimitWindows.java
+++ sharedPrompts/src/main/java/org/example/sharedprompts/auth/rate/policy/RateLimitWindows.java
@@ -27,7 +27,7 @@ public class RateLimitWindows {
      * 회원가입 윈도우 시간 (초)
      */
     @Min(1)
-    private long signupSeconds = 3600L;
+    private long signupSeconds = 60L;
 
     /**
      * OAuth2 로그인 확정 윈도우 시간 (초)
