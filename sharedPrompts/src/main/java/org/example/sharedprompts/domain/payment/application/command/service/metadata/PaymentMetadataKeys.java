f93cf450349c9d9be4e41dcd61cbd4dba1f2631a
rabitis99 <kyungsu9914@gmail.com>
2026-02-06 11:06:47 +0900

    Private 생성자가 인스턴스화를 방지하지만, final 키워드를 추가하면 서브클래싱도 명시적으로 방지하여 유틸리티 클래스의 의도가 더 명확해집니다.

--- sharedPrompts/src/main/java/org/example/sharedprompts/domain/payment/application/command/service/metadata/PaymentMetadataKeys.java
+++ sharedPrompts/src/main/java/org/example/sharedprompts/domain/payment/application/command/service/metadata/PaymentMetadataKeys.java
@@ -1,6 +1,6 @@
 package org.example.sharedprompts.domain.payment.application.command.service.metadata;
 
-public class PaymentMetadataKeys {
+public final class PaymentMetadataKeys {
     public static final String PRODUCT_NAME = "product_name";
     public static final String NEXT_REDIRECT_PC_URL = "next_redirect_pc_url";
     public static final String REDIRECT_URL = "redirect_url";
